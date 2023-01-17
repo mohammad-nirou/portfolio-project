2c35cf4ccd89689d76d3c7cf6089fbd153a78c8d
mohammad niroujazi <mohammad.nj@jacobb.ai>
2023-01-17 00:36:22 -0500

    experience added my info

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,27 +10,19 @@ const Experience = () => {
         <div className="timeline-block timeline-block-right">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2014-2015</h3>
+            <h3>2013-2018</h3>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
-              nam quae quod laboriosam vero dolorem, porro suscipit at itaque
-              aspernatur voluptatem dignissimos, fuga ducimus veniam. Quas,
-              perferendis tenetur magni accusamus enim maiores odio, aperiam,
-              hic suscipit aliquam dicta officia sunt.
-            </p>
+              I did my Bachelors degree in Electrical & Computer Engineering from the University of Birjand. I was a member of the university's robotics team and participated in many competitions. I also received the full tuition fee waiver scholarship from the university.
+              </p>
           </div>
         </div>
         {/* - */}
         <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2015-2017</h3>
+            <h3>2017-2019</h3>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
-              nam quae quod laboriosam vero dolorem, porro suscipit at itaque
-              aspernatur voluptatem dignissimos, fuga ducimus veniam. Quas,
-              perferendis tenetur magni accusamus enim maiores odio, aperiam,
-              hic suscipit aliquam dicta officia sunt.
+              I worked as an IT consultant and web developer at a local company. I was responsible for developing and maintaining the company's website and other web applications. I also worked on the company's internal network and servers.
             </p>
           </div>
         </div>
@@ -38,26 +30,18 @@ const Experience = () => {
         <div className="timeline-block timeline-block-right">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2018-2019</h3>
+            <h3>2020-2023</h3>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
-              nam quae quod laboriosam vero dolorem, porro suscipit at itaque
-              aspernatur voluptatem dignissimos, fuga ducimus veniam. Quas,
-              perferendis tenetur magni accusamus enim maiores odio, aperiam,
-              hic suscipit aliquam dicta officia sunt.
+              I completed my Masters degree in Computer Science in Sherbrooke, Quence, Canada. I worked on several interesting projects during my studies. I also worked as a teaching assistant for the university's courses. I also received several scholarship from the university. For my thesis project, I worked on a project to detect and classify the trajectory of pedestrians in a video using deep learning and computer vision techniques. I passed several courses about software engineering, computer vision, machine learning. I also learned about the latest technologies in the field of computer science.
             </p>
           </div>
         </div>
         {/* - */}<div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2019-2022</h3>
+            <h3>2021-2023</h3>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
-              nam quae quod laboriosam vero dolorem, porro suscipit at itaque
-              aspernatur voluptatem dignissimos, fuga ducimus veniam. Quas,
-              perferendis tenetur magni accusamus enim maiores odio, aperiam,
-              hic suscipit aliquam dicta officia sunt.
+              I work as an Artificial Intelligence Developer / Researcher Engineer at a local company based in Montreal. I mainly use Python programming language to develop and maintain the company's software. I also work on the company's internal network and servers. I use state-of-the-art technologies such as Docker, Git, GitHub, BitBucket to develop and maintain the company's software. I also use the latest technologies in the field of computer science such as machine learning, deep learning, computer vision, and natural language processing. 
             </p>
           </div>
         </div>
@@ -65,13 +49,9 @@ const Experience = () => {
         <div className="timeline-block timeline-block-right">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2022-2023</h3>
+            <h3>2021-2021</h3>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
-              nam quae quod laboriosam vero dolorem, porro suscipit at itaque
-              aspernatur voluptatem dignissimos, fuga ducimus veniam. Quas,
-              perferendis tenetur magni accusamus enim maiores odio, aperiam,
-              hic suscipit aliquam dicta officia sunt.
+              I worked as an Technical IT Support at a local company based in Montreal. During this period which lasted for 7 months, I was in directs contact with the company's clients for 8 hours per day and I was responsible for solving their problems. I also worked on the company's internal network and servers. It really helped me to improve my communication skills and my problem-solving skills. Also it gave me the confidence to work with clients with different backgrounds and cultures and English accents which helped me to improve my English skills as well. I also learned about the latest technologies in the field of computer science. I had access to lots of free resources which I used to improve my skills in the field of computer science in my free time after the working hours.
             </p>
           </div>
         </div>
@@ -79,14 +59,10 @@ const Experience = () => {
         <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2022-2023</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
-              nam quae quod laboriosam vero dolorem, porro suscipit at itaque
-              aspernatur voluptatem dignissimos, fuga ducimus veniam. Quas,
-              perferendis tenetur magni accusamus enim maiores odio, aperiam,
-              hic suscipit aliquam dicta officia sunt.
-            </p>
+          <h3>2020-2023</h3>
+          <p>
+            I completed my Masters degree in Computer Science in Sherbrooke, Quence, Canada. I worked on several interesting projects during my studies. I also worked as a teaching assistant for the university's courses. I also received several scholarship from the university. For my thesis project, I worked on a project to detect and classify the trajectory of pedestrians in a video using deep learning and computer vision techniques. I passed several courses about software engineering, computer vision, machine learning. I also learned about the latest technologies in the field of computer science.
+          </p>
           </div>
         </div>
 
