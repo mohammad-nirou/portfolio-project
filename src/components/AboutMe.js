634dcf815de4c72a8683d622ec376c4b3a4d0096
mohammad niroujazi <mohammad.nj@jacobb.ai>
2023-01-19 15:01:54 -0500

    about me content

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,8 +16,7 @@ const AboutMe = () => {
             <div className='col-lg-6 col-xm-12'>
                 <h1 className='about-heading'>About me</h1>
                 <p >
-                I'm passionate about Web Development, and Artifitial Intelligence. I enjoy providing AI-based solutions to a wide range of issues, developing websites, and working with new technologies that facilitate communication, access to data, and help people. I am a self-motivated, hard-working, and a quick learner. I am always looking for new opportunities to learn and grow. I am a team player and I am always open to new ideas and suggestions. I am a fast learner and I am always looking for new opportunities to learn and grow. I have an M.Sc. in Computer Science and I am actively learning new skills and working on the ones already acquired. 
-
+                I'm a  passionate developer. I'm a computer science graduate. Web development and artificial intelligence are two topics that strongly interest me. I enjoy creating websites, offering AI-based solutions to a variety of problems, and working on cutting-edge technologies that improve accessibility and human communication. I am a person who is self-motivated, diligent, and quick to pick things up. I'm constantly searching for new possibilities to develop my technical and interpersonal abilities as well as learn new ones.
 
                 </p>
 
